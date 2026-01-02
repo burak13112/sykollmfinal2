85a2d6265bcf67fb874c7245733e95ae8e915f92
burak13112 <141680267+burak13112@users.noreply.github.com>
2026-01-02 20:54:42 +0300

    Add files via upload

---
+++ components/Icon.tsx
@@ -0,0 +1,29 @@
+import { 
+  Terminal, 
+  Moon, 
+  Sun, 
+  Trash2, 
+  Plus, 
+  Send, 
+  Cpu, 
+  MessageSquare, 
+  Save, 
+  ChevronDown,
+  Menu,
+  X
+} from 'lucide-react';
+
+export const Icons = {
+  Terminal,
+  Moon,
+  Sun,
+  Trash: Trash2,
+  Plus,
+  Send,
+  Cpu,
+  Chat: MessageSquare,
+  Save,
+  ChevronDown,
+  Menu,
+  Close: X
+};
